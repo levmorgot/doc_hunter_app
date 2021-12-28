fc9eb2a7c4ad979f3d93de93fdc5e49c93095110
levmorgot <levmorgot@gmail.com>
2021-12-28 13:24:46 +0300

    уровень придставления (cubit  для вывода всех больниц) (филиалы)

---
+++ lib/filials/presentation/bloc/filials_list_cubit/filails_list_cubit.dart
@@ -0,0 +1,48 @@
+import 'package:doc_hunter_app/core/error/failure.dart';
+import 'package:doc_hunter_app/filials/domain/entities/filial_entity.dart';
+import 'package:doc_hunter_app/filials/domain/usecases/get_all_filials.dart';
+import 'package:doc_hunter_app/filials/domain/usecases/params/page_filial_params.dart';
+import 'package:doc_hunter_app/filials/presentation/bloc/filials_list_cubit/filials_list_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class FilialsListCubit extends Cubit<FilialState> {
+  final GetAllFilials getAllFilials;
+  final int limit;
+
+  FilialsListCubit({required this.getAllFilials, required this.limit}) : super(FilialEmptyState());
+  int skip = 0;
+
+  void loadFilials() async {
+    if (state is FilialLoadingState) return;
+
+    final currentState = state;
+
+    var oldFilials = <FilialEntity>[];
+    if (currentState is FilialLoadedState) {
+      oldFilials = currentState.filialsList;
+    }
+    
+    emit(FilialLoadingState(oldFilials, isFirstFetch: skip == 0));
+
+    final failureOrFilials = await getAllFilials(PageFilialParams(skip: skip, limit: limit));
+
+    failureOrFilials.fold((failure) => FilialErrorState(message: _mapFailureMessage(failure)), (filial) {
+      skip += limit;
+      final filials = (state as FilialLoadingState).oldFilialsList;
+      filials.addAll(filial);
+      emit(FilialLoadedState(filials));
+    });
+  }
+
+  String _mapFailureMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return 'Server failure';
+      case CacheFailure:
+        return 'Cache failure';
+      default:
+        return 'Unexpected Error';
+    }
+  }
+
+}
---
+++ lib/filials/presentation/bloc/filials_list_cubit/filials_list_state.dart
@@ -0,0 +1,43 @@
+import 'package:doc_hunter_app/filials/domain/entities/filial_entity.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class FilialState extends Equatable {
+  const FilialState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class FilialEmptyState extends FilialState {
+  @override
+  List<Object> get props => [];
+}
+
+class FilialLoadingState extends FilialState {
+  final List<FilialEntity> oldFilialsList;
+  final bool isFirstFetch;
+
+  const FilialLoadingState(this.oldFilialsList, {this.isFirstFetch = false});
+
+  @override
+  List<Object> get props => [oldFilialsList];
+}
+
+
+class FilialLoadedState extends FilialState {
+  final List<FilialEntity> filialsList;
+
+  const FilialLoadedState(this.filialsList);
+
+  @override
+  List<Object> get props => [filialsList];
+}
+
+class FilialErrorState extends FilialState {
+  final String message;
+
+  const FilialErrorState({required this.message});
+
+  @override
+  List<Object> get props => [message];
+}
