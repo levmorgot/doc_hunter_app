76c7db452ca1eb6f2f4b808e2591ba6b928ba3ed
levmorgot <levmorgot@gmail.com>
2022-01-10 17:07:09 +0300

    получение всех данных о врачах напрямую из приложения

--- lib/doctors/data/models/doctor_model.dart
+++ lib/doctors/data/models/doctor_model.dart
@@ -1,13 +1,26 @@
 import 'package:doc_hunter_app/doctors/domain/entities/doctor_entity.dart';
 
 class DoctorModel extends DoctorEntity {
-  const DoctorModel({required id, required name})
-      : super(id: id, name: name);
+  const DoctorModel(
+      {required id,
+      required name,
+      required departmentId,
+      required filialId,
+      required filialCashId})
+      : super(
+            id: id,
+            name: name,
+            departmentId: departmentId,
+            filialId: filialId,
+            filialCashId: filialCashId);
 
   factory DoctorModel.fromJson(Map<String, dynamic> json) {
     return DoctorModel(
       id: json['id'] ?? json['dcode'],
       name: json['name'],
+      departmentId: json['departmentId'] ?? -1,
+      filialId: json['filialId'] ?? -1,
+      filialCashId: json['filialCashId'] ?? json['cashId'] ?? -1,
     );
   }
 
@@ -15,6 +28,9 @@ class DoctorModel extends DoctorEntity {
     return {
       'id': id,
       'name': name,
+      'departmentId': departmentId,
+      'filialId': filialId,
+      'filialCashId': filialCashId,
     };
   }
 }
--- lib/doctors/domain/entities/doctor_entity.dart
+++ lib/doctors/domain/entities/doctor_entity.dart
@@ -3,12 +3,18 @@ import 'package:equatable/equatable.dart';
 class DoctorEntity extends Equatable {
   final int id;
   final String name;
+  final int departmentId;
+  final int filialId;
+  final int filialCashId;
 
   const DoctorEntity({
     required this.id,
     required this.name,
+    required this.departmentId,
+    required this.filialId,
+    required this.filialCashId,
   });
 
   @override
-  List<Object?> get props => [id, name];
+  List<Object?> get props => [id, name, departmentId, filialId, filialCashId];
 }
